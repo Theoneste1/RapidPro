120f84e2d16a9a82bd185ac29105c4a3f798defa
Theoneste Nsanzabarinda <Nsanzabarinda.Theoneste@tek-experts.com>
2023-06-13 01:43:12 +0200

    added file

--- temba_get_request.py
+++ temba_get_request.py
@@ -1,4 +1,5 @@
 import json
+import csv
 from temba_client.v2 import TembaClient
 client = TembaClient('https://rapidpro-next.idems.international', '8d85587d8f67eeea194d305f926b4f96841df10e')
 
@@ -8,5 +9,18 @@ for contact_batch in client.get_contacts(group='everyone').iterfetches():
         # Here I'm just print some of the stats from each student.
         # You can see the relevant fields by looking at one of the students in RapidPro: https://rapidpro-next.idems.international/contact/read/2e05e0c3-c90f-4d8f-b711-53c9d2ff361b/
         # Name is the Student's Telegram name; studentname is the name the student gave us during registration
-        # print([c.get('name'), c.get('fields').get('studentname'), c.get('fields').get('next_pscl'), c.get('fields').get('open_pscl')])
-        print([c.get('name'), c.get('fields').get('studentname'), c.get('fields').get('next_data'),c.get('fields').get('open_data')])
+        student=[c.get('name'), c.get('fields').get('studentname'), c.get('fields').get('next_pscl'), c.get('fields').get('open_pscl')]
+        # print(student)
+        # print([c.get('name'), c.get('fields').get('studentname'), c.get('fields').get('next_data'),c.get('fields').get('open_data')])
+
+        student=[c.get('name'), c.get('fields').get('studentname'), 
+                 c.get('fields').get('next_pscl'), c.get('fields').get('open_pscl'),
+                 c.get('fields').get('next_combinatorics'), c.get('fields').get('open_combinatorics'),
+                 c.get('fields').get('next_algebra'), c.get('fields').get('open_algebra'),
+                 c.get('fields').get('next_numbers'), c.get('fields').get('open_numbers'),
+                 c.get('fields').get('next_data'), c.get('fields').get('open_data'),
+                 c.get('fields').get('next_geometry'), c.get('fields').get('open_geometry')
+                 
+                 
+                 ]
+        print(student)
